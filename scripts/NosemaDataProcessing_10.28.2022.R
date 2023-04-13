1bbf2db260a1ec360ed2cb2fff98ba1dd88f652f
mlfearon <mlfearon@umich.edu>
2023-04-13 14:38:42 -0400

    adding in here package to script

--- scripts/NosemaDataProcessing_10.28.2022.R
+++ scripts/NosemaDataProcessing_10.28.2022.R
@@ -7,14 +7,12 @@
 
 
 # Written by: Michelle Fearon and Maryellen Zbrozek
-# Last updated: 28 October 2022
+# Last updated: 13 April 2023
 
 
 
 
-rm(list = ls())
 # set the working directory
-setwd("C:/Users/mlfearon/OneDrive - Umich/PROJECTS/Manuscripts/Nosema and visitation patterns/Data and Codes")
 setwd("C:/Users/Maryellen/Documents/School/Honors Thesis/Manuscript/Analysis")
 
 
@@ -23,10 +21,14 @@ library(dplyr)
 library(ggplot2)
 library(vegan)
 library(epiR)
+library(here)
+
+# set the path to the script relative to the project root directory
+here::i_am("scripts/NosemaAnalysis_2.20.2023.R")
 
 
 # load binary nosema data
-nosema <- read.csv("Nosema_pos_update_missing18Sremoved.csv", stringsAsFactors = F)
+nosema <- read.csv("data/Nosema_pos_update_missing18Sremoved.csv", stringsAsFactors = F)
 head(nosema)
 summary(nosema)
 dim(nosema)
@@ -50,7 +52,7 @@ unique(nosema$Site)
 
 
 ###### add in visitation covariates to data set 
-visitation <- read.csv("Video_Summary_2016_forNosemaAnalysis.csv", stringsAsFactors = F)
+visitation <- read.csv("data/Video_Summary_2016_forNosemaAnalysis.csv", stringsAsFactors = F)
 summary(visitation)
 
 
@@ -58,7 +60,7 @@ summary(visitation)
 visitation <- visitation %>%
   select(FlowerID, Site, Year, Visit, Date, StartHour, totdur_min:Other_dur, APIS_dur2:Other_dur5) %>%
   mutate(APIS_freq = if_else(VisitNum == 0, 0, APIS_visits/VisitNum), BOMB_freq = if_else(VisitNum == 0, 0, BOMB_visits/VisitNum),
-         Native_freq = if_else(VisitNum == 0, 0, Native_visits/VisitNum), Other_freq = if_else(VisitNum == 0, 0, Other_visits/VisitNum),
+         Native_freq = if_else(VisitNum == 0, 0, Native_visits/VisitNum), Other_freq = if_else(VisitNum == 0, 0, Other_visits/VisitNum), Other_rate = Other_visits/totdur_min,
          APIS_visitdur = if_else(APIS_visits == 0, 0, APIS_dur/APIS_visits), APIS_visitdur2 = if_else(APIS_visits == 0, 0, APIS_dur2/APIS_visits),
          APIS_visitdur3 = if_else(APIS_visits == 0, 0, APIS_dur3/APIS_visits), APIS_visitdur4 = if_else(APIS_visits == 0, 0, APIS_dur4/APIS_visits),
          APIS_visitdur5 = if_else(APIS_visits == 0, 0, APIS_dur5/APIS_visits), BOMB_visitdur = if_else(BOMB_visits == 0, 0, BOMB_dur/BOMB_visits),
@@ -75,7 +77,11 @@ visitation <- visitation %>%
 # visitdur4 = duration on pollen only
 # visitdur5 = duration on pollen and nectar simultaneously
 
-
+# total time in minutes of each video recording
+mean(visitation$totdur_min)
+median(visitation$totdur_min)
+sd(visitation$totdur_min)
+hist(visitation$totdur_min)
 
 # calculate shannon index per flower
 spp_visits <- visitation %>%
@@ -91,9 +97,9 @@ View(visitation)
 avgs <- visitation %>%
   group_by(Site, Visit) %>%
   summarize(VisitDur = mean(VisitDur), VisitNum = mean(VisitNum), VisitRichnessPerFlower = mean(VisitRichness),
-            APIS_visits = mean(APIS_visits), BOMB_visits = mean(BOMB_visits),
-            APIS_freq = mean(APIS_freq), BOMB_freq = mean(BOMB_freq), Native_visits = mean(Native_visits), 
-            Other_visits = mean(Other_visits), Native_freq = mean(Native_freq), Other_freq = mean(Other_freq), 
+            APIS_visits = mean(APIS_visits), BOMB_visits = mean(BOMB_visits), Other_visits = mean(Other_visits), Native_visits = mean(Native_visits),
+            APIS_freq = mean(APIS_freq), BOMB_freq = mean(BOMB_freq), Other_freq = mean(Other_freq), Native_freq = mean(Native_freq),
+            APIS_rate = mean(APIS_rate), BOMB_rate = mean(BOMB_rate), Other_rate = mean(Other_rate),
             APIS_dur = mean(APIS_dur), APIS_dur2 = mean(APIS_dur2), APIS_dur3 = mean(APIS_dur3), APIS_dur4 = mean(APIS_dur4),
             APIS_dur5 = mean(APIS_dur5), APIS_visitdur = mean(APIS_visitdur), APIS_visitdur2 = mean(APIS_visitdur2), 
             APIS_visitdur3 = mean(APIS_visitdur3), APIS_visitdur4 = mean(APIS_visitdur4), APIS_visitdur5 = mean(APIS_visitdur5),
@@ -125,7 +131,7 @@ unique(data$Site)
 
 
 # Data file to be used in analyses
-write.csv(data, "NosemaAnalysis_17Oct2022_18Sremoved.csv", quote = F)
+write.csv(data, "data/NosemaAnalysis_18Sremoved.csv", quote = F)
 
 
 
@@ -138,6 +144,9 @@ avgs_perSite <- visitation %>%
             APIS_visits_mean = mean(APIS_visits), APIS_visits_min = min(APIS_visits), APIS_visits_max = max(APIS_visits), 
             BOMB_visits_mean = mean(BOMB_visits), BOMB_visits_min = min(BOMB_visits), BOMB_visits_max = max(BOMB_visits),
             Other_visits_mean = mean(Other_visits), Other_visits_min = min(Other_visits), Other_visits_max = max(Other_visits),
+            APIS_rate_mean = mean(APIS_rate), APIS_rate_min = min(APIS_rate), APIS_rate_max = max(APIS_rate), 
+            BOMB_rate_mean = mean(BOMB_rate), BOMB_rate_min = min(BOMB_rate), BOMB_rate_max = max(BOMB_rate),
+            Other_rate_mean = mean(Other_rate), Other_rate_min = min(Other_rate), Other_rate_max = max(Other_rate),
             APIS_visitdur_mean = mean(APIS_visitdur), APIS_visitdur_min = min(APIS_visitdur), APIS_visitdur_max = max(APIS_visitdur),
             BOMB_visitdur_mean = mean(BOMB_visitdur), BOMB_visitdur_min = min(BOMB_visitdur), BOMB_visitdur_max = max(BOMB_visitdur),
             Other_visitdur_mean = mean(Other_visitdur), Other_visitdur_min = min(Other_visitdur), Other_visitdur_max = max(Other_visitdur),
@@ -157,7 +166,30 @@ View(avgs_perSite)
 
 
 # export file to make the table
-write.csv(avgs_perSite, "MeanBeeVisitationPerSite_17Oct2022.csv", quote = F)
+write.csv(avgs_perSite, "tables/MeanBeeVisitationPerSite.csv", quote = F)
+
+
+
+##############################
+# Manipulate visitation data set to analyze differences in visitation by species
+#############################
+
+# Data collected per flower
+dim(visitation)
+visitation_bySpp <- visitation %>%
+  pivot_longer()
+
+
+
+
+
+
+###############################
+# Tests of differences in visitation metrics between species (Apis, Bombus, Other)
+# For each type of visitation metric, compare whether we get the same results from the per flower (as collected) and averaged per site/visit and per site
+##############################
+
+
 
 
 
